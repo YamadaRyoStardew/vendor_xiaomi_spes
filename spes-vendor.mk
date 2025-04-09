64bd2edbef862a2830174e98ffa48930a925e1b2
angelomds42 <angelomds42@gmail.com>
2025-04-10 00:27:07 +0200

    spes: Sync with device tree
    - Disable ELF checks for some libraries
    - Remove dead OMX packages
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- spes-vendor.mk
+++ spes-vendor.mk
@@ -419,7 +419,6 @@ PRODUCT_PACKAGES += \
     libscveObjectTracker \
     libscveObjectTracker_stub \
     libsdsprpc \
-    libswvdec \
     libsysmon_cdsp_skel \
     libthermalclient \
     vendor.qti.hardware.dsp@1.0 \
@@ -431,17 +430,6 @@ PRODUCT_PACKAGES += \
     audio.primary.bengal \
     vendor.qti.hardware.bluetooth_audio@2.0-impl \
     vendor.qti.hardware.bluetooth_audio@2.1-impl \
-    libOmxAacDec \
-    libOmxAlacDec \
-    libOmxAmrDec \
-    libOmxAmrwbplusDec \
-    libOmxApeDec \
-    libOmxDsdDec \
-    libOmxEvrcDec \
-    libOmxG711Dec \
-    libOmxQcelp13Dec \
-    libOmxVideoDSMode \
-    libOmxWmaDec \
     liba2dpoffload \
     libacdb-fts \
     libacdbloader \
@@ -460,7 +448,6 @@ PRODUCT_PACKAGES += \
     libcirrusspkrprot \
     libcomprcapture \
     libdrc \
-    libdsd2pcm \
     libexthwplugin \
     libfastcrc \
     libgcs-calwrapper \
@@ -828,6 +815,7 @@ PRODUCT_PACKAGES += \
     libst_sr_models \
     libstfactory-vendor \
     libswregistrationalgo \
+    libswvdec \
     libsynergy_loc_api \
     libsystem_health_mon \
     libtfestriping \
